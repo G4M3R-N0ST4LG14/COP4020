d56ebd087e074ba23e1e51eb75ac5676f7a36b1e
G4M3R-N0ST4LG14 <laraarturo2001@gmail.com>
2025-05-15 10:41:08 -0400

    Update secondAssignment.hs

--- Haskell/Assignment_2/secondAssignment.hs
+++ Haskell/Assignment_2/secondAssignment.hs
@@ -1,42 +1,46 @@
--- Arturo Lara
+-- Assignment 2: Functions and List Comprehensions
 
--- 1. Create a function named problem1. Use a list comprehension for the main function. This function will take a single integer parameter. It indicates the number of primes that will be the final list. The final solution will output a list of every other prime.
+-- 1. Function that will take a single integer parameter. It indicates the number of primes that will be the final list. The final solution will output a list of every other prime.
 -- Helper function to check if a number is a prime
 isPrime :: Int -> Bool
 isPrime x
     | x < 2 = False -- Numbers less than 2 are not prime numbers
     | x == 2 = True -- 2 is a prime number
-    | x `mod` 2 == 0 = False -- Even numbers are not prime numbers
-    | otherwise = null [y | y <- [3,5..floor (sqrt (fromIntegral x))], x `mod` y ==0] -- Checks to see if any odd numbers up to the square root of x can divide the number to see if it's prime
+    | even x = False -- Even numbers are not prime numbers
+    -- Checks to see if any odd numbers up to the square root of x can divide the number to see if it's prime
+    | otherwise = null [y | y <- [3,5..floor (sqrt (fromIntegral x))], x `mod` y ==0] 
 
 -- Main function to generate the list of every other prime
 primes = filter isPrime [2..] -- Create an infinite list of primes
 problem1 :: Int -> [Int]
-problem1 n = take n [x | (x, y) <- zip primes [0..], even y] -- Put primes in an index where the even indices starting at 0 get added to the final list and the odds get filtered
+-- Put primes in an index where the even indices starting at 0 get added to the final list and the odds get filtered
+problem1 n = take n [x | (x, y) <- zip primes [0..], even y] 
 
 
--- 2. Create a function named problem2. Use recursion to solve this problem. This function will take a single integer parameter. It indicates the maximum value of Fibonacci numbers that will be in the list. The final solution will be a list of Fibonacci numbers that have a three as their right-most digit and are less than or equal to n.
+-- 2. Function to indicate the maximum value of Fibonacci numbers that have a three as their right-most digit and are less than or equal to n.
 problem2 :: Int -> [Int]
-generateFibs a b = a : generateFibs b (a + b) -- Creates Fibonacci sequence with recursion by adding a and b together and creating an infinite list where b is the next starting number in the addition
+-- Creates Fibonacci sequence with recursion by adding a and b together and creating an infinite list where b is the next starting number in the addition
+generateFibs a b = a : generateFibs b (a + b) 
 fibs = generateFibs 1 2 -- Starts Fibonacci sequence at 1 and 2
 hasThree x = x `mod` 10 == 3 -- Checks to see if the final digit is 3
 problem2 n = filter hasThree (takeWhile (<= n) fibs) -- Filters out numbers from list that don't end with 3
 
 
--- 3. Create a function named problem3. For this problem, you can solve it any way you’d like. You may (and should) use helper functions. Create a list from numbers from 1 to n. The numbers must be either a multiple of five or have exactly three factors.
+-- 3. Function that finds numbers that are a multiple of five or have exactly three factors.
 problem3 :: Int -> [Int]
-isPerfectSquare x = (floor (sqrt (fromIntegral x)))^2 == x -- Check to see if square root of x is a whole number
+isPerfectSquare x = floor (sqrt (fromIntegral x))^2 == x -- Check to see if square root of x is a whole number
 isMultipleOfFive x = x `mod` 5 == 0 -- Check to see if x is a multiple of 5
-hasExactlyThreeFactors x = isPerfectSquare x && isPrime (floor (sqrt (fromIntegral x))) -- If the number x has a whole square root and the square root is a prime number then it is guaranteed to have only 3 factors (1, sqrt(x), and x)
+-- If the number x has a whole square root and the square root is a prime number then it is guaranteed to have only 3 factors (1, sqrt(x), and x)
+hasExactlyThreeFactors x = isPerfectSquare x && isPrime (floor (sqrt (fromIntegral x))) 
 problem3 n = [x | x <- [2..n], isMultipleOfFive x || hasExactlyThreeFactors x] -- Gets all numbers up to n that are either multiples of five or have exactly three factors
 
 main :: IO ()
 main = do
-    putStrLn("1. Looking for every other prime number until reaching 10 and 7 total numbers: ")
+    putStrLn"1. Looking for every other prime number until reaching 10 and 7 total numbers: "
     print(problem1 10)
     print(problem1 7)
-    putStrLn("2. Filtering out the Fibonacci numbers that end with the digit 3 up to 100 and 10000: ")
+    putStrLn"2. Filtering out the Fibonacci numbers that end with the digit 3 up to 100 and 10000: "
     print(problem2 100)
     print(problem2 10000)
-    putStrLn("3. Generate a list of the multiples of 5 and numbers with exactly 3 factors: ")
+    putStrLn"3. Generate a list of the multiples of 5 and numbers with exactly 3 factors: "
     print(problem3 100)
