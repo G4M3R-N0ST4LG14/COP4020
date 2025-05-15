149020eb41bf2afede1705c2cdb7346d47a7d829
G4M3R-N0ST4LG14 <laraarturo2001@gmail.com>
2025-05-15 11:51:34 -0400

    Update test.hs

--- Haskell/Haskell_Test/test.hs
+++ Haskell/Haskell_Test/test.hs
@@ -1,3 +1,7 @@
 -- Haskell Test
 
 -- 1. Function that creates a list from 1 to n and prints out the even numbers that are multiples of 5 with first digit of 6
+solveProblem1 :: Int -> [Int]
+generateList n = [x | x <- [1..n], x `mod` 5 == 0 && head (show x) == '6']
+-- Filter out even numbers
+solveProblem1 n = filter even (generateList n)
